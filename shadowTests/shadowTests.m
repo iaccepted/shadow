ded8bc7d6418a3c250ad9b18028f9ef2fd63dca7
guohongzhi <succeed.2009@163.com>
2015-05-07 12:46:08 +0800

    Initial Commit

---
+++ shadowTests/shadowTests.m
@@ -0,0 +1,40 @@
+//
+//  shadowTests.m
+//  shadowTests
+//
+//  Created by iaccepted on 15/5/7.
+//  Copyright (c) 2015年 iaccepted. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface shadowTests : XCTestCase
+
+@end
+
+@implementation shadowTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
