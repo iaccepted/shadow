31e457f177bf192c61cab8ad55dc7a97e0179c72
guohongzhi <succeed.2009@163.com>
2015-07-28 16:47:34 +0800

    工程文件结构的大调整

---
+++ shadow/classes/controllers/SkinViewController.h
@@ -0,0 +1,13 @@
+//
+//  SkinViewController.h
+//  shadow
+//
+//  Created by iaccepted on 15/7/28.
+//  Copyright (c) 2015年 iaccepted. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SkinViewController : UIViewController
+
+@end
---
+++ shadow/classes/controllers/SkinViewController.m
@@ -0,0 +1,13 @@
+//
+//  SkinViewController.m
+//  shadow
+//
+//  Created by iaccepted on 15/7/28.
+//  Copyright (c) 2015年 iaccepted. All rights reserved.
+//
+
+#import "SkinViewController.h"
+
+@implementation SkinViewController
+
+@end
